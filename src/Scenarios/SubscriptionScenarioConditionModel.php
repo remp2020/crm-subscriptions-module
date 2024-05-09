a18809812baf920ab5cd64a3a7b9fc1cb23ee05a
Peter Burian <peter.burian@dennikn.sk>
2024-05-09 13:05:15 +0000

    All ScenarioConditionModel - fix wrong exceptions
    remp/crm#1409

--- src/Scenarios/SubscriptionScenarioConditionModel.php
+++ src/Scenarios/SubscriptionScenarioConditionModel.php
@@ -5,8 +5,8 @@ namespace Crm\SubscriptionsModule\Scenarios;
 use Crm\ApplicationModule\Models\Criteria\ScenarioConditionModelInterface;
 use Crm\ApplicationModule\Models\Criteria\ScenarioConditionModelRequirementsInterface;
 use Crm\ApplicationModule\Models\Database\Selection;
+use Crm\ScenariosModule\Events\ConditionCheckException;
 use Crm\SubscriptionsModule\Repositories\SubscriptionsRepository;
-use Exception;
 
 class SubscriptionScenarioConditionModel implements ScenarioConditionModelInterface, ScenarioConditionModelRequirementsInterface
 {
@@ -23,7 +23,7 @@ class SubscriptionScenarioConditionModel implements ScenarioConditionModelInterf
     public function getItemQuery($scenarioJobParameters): Selection
     {
         if (!isset($scenarioJobParameters->subscription_id)) {
-            throw new Exception("Subscription scenario conditional model requires 'subscription_id' job param.");
+            throw new ConditionCheckException("Subscription scenario conditional model requires 'subscription_id' job param.");
         }
 
         return $this->subscriptionsRepository->getTable()->where(['subscriptions.id' => $scenarioJobParameters->subscription_id]);
