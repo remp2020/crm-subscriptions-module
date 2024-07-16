454d49ef83880cf374cc4777eed8e7f79986c81e
Miroslav Svitok <mirosvitok@gmail.com>
2024-07-16 11:29:51 +0000

    Add One Stop Shop resolver to PaymentsRepository#copyPayment and to payment upgrades
    - new PaymentItemContainerFactory is created, which can create payment items out of existing 
    payment
    - each payment item type should now register itself in container
    
    remp/crm#3246

--- src/Models/PaymentItem/SubscriptionTypePaymentItem.php
+++ src/Models/PaymentItem/SubscriptionTypePaymentItem.php
@@ -6,34 +6,26 @@ use Crm\PaymentsModule\Models\PaymentItem\PaymentItemInterface;
 use Crm\PaymentsModule\Models\PaymentItem\PaymentItemTrait;
 use Nette\Database\Table\ActiveRow;
 
-class SubscriptionTypePaymentItem implements PaymentItemInterface
+final class SubscriptionTypePaymentItem implements PaymentItemInterface
 {
     use PaymentItemTrait;
 
     public const TYPE = 'subscription_type';
 
-    private $meta;
-
-    private $subscriptionTypeId;
-
-    private $subscriptionTypeItemId;
-
     public function __construct(
-        int $subscriptionTypeId,
+        private int $subscriptionTypeId,
         string $name,
         float $price,
         int $vat,
         int $count = 1,
         array $meta = [],
-        ?int $subscriptionTypeItemId = null
+        private ?int $subscriptionTypeItemId = null,
     ) {
-        $this->subscriptionTypeId = $subscriptionTypeId;
         $this->name = $name;
         $this->price = $price;
         $this->vat = $vat;
         $this->count = $count;
         $this->meta = $meta;
-        $this->subscriptionTypeItemId = $subscriptionTypeItemId;
     }
 
     /**
@@ -72,22 +64,32 @@ class SubscriptionTypePaymentItem implements PaymentItemInterface
      * @return SubscriptionTypePaymentItem
      * @throws \Exception Thrown if payment item isn't `subscription_type` payment item type.
      */
-    public static function fromPaymentItem(ActiveRow $paymentItem)
+    public static function fromPaymentItem(ActiveRow $paymentItem): static
     {
         if ($paymentItem->type !== self::TYPE) {
             throw new \Exception("Can not load SubscriptionTypePaymentItem from payment item of different type. Got [{$paymentItem->type}]");
         }
-        $meta = $paymentItem->related('payment_item_meta')->fetchPairs('key', 'value');
-
         return new SubscriptionTypePaymentItem(
             $paymentItem->subscription_type_id,
             $paymentItem->name,
             $paymentItem->amount,
             $paymentItem->vat,
             $paymentItem->count,
-            $meta,
+            self::loadMeta($paymentItem),
             $paymentItem->subscription_type_item_id
         );
+    }
+
+    public function forceName($name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function forcePrice(int $price): self
+    {
+        $this->price = $price;
+        return $this;
     }
 
     public function data(): array
@@ -96,28 +98,5 @@ class SubscriptionTypePaymentItem implements PaymentItemInterface
             'subscription_type_id' => $this->subscriptionTypeId,
             'subscription_type_item_id' => $this->subscriptionTypeItemId,
         ];
-    }
-
-    public function meta(): array
-    {
-        return $this->meta;
-    }
-
-    public function forceName(string $name): self
-    {
-        $this->name = $name;
-        return $this;
-    }
-
-    public function forceVat(int $vat): static
-    {
-        $this->vat = $vat;
-        return $this;
-    }
-
-    public function forcePrice(float $price): self
-    {
-        $this->price = $price;
-        return $this;
     }
 }
